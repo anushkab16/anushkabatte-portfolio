1b314ae93cabaa5182b3375ab2520fac477b7fcf
anushkab16 <anushkabatte1612@gmail.com>
2026-08-03 18:10:01 +0530

    hero and about section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from './components/Hero'
 import { About } from './components/About'
+import { Projects } from './components/Projects'
 
 export default function Page() {
   return (
     <>
       <Hero />
       <About />
+      <Projects />
     </>
   )
 }
